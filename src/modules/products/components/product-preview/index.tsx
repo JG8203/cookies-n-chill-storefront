383259e9a15a22d3b8494818ea4e41b34a24a0f7
JG8203 <joshua_gilo@dlsu.edu.ph>
2024-06-15 21:32:48 +0800

    nagayos lang ng stuff

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,36 +1,32 @@
-import { Text } from "@medusajs/ui"
+import { Text } from "@medusajs/ui";
+import { ProductPreviewType } from "types/global";
+import { retrievePricedProductById } from "@lib/data";
+import { getProductPrice } from "@lib/util/get-product-price";
+import { Region } from "@medusajs/medusa";
+import LocalizedClientLink from "@modules/common/components/localized-client-link";
+import Thumbnail from "../thumbnail";
+import PreviewPrice from "./price";
 
-import { ProductPreviewType } from "types/global"
-
-import { retrievePricedProductById } from "@lib/data"
-import { getProductPrice } from "@lib/util/get-product-price"
-import { Region } from "@medusajs/medusa"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import Thumbnail from "../thumbnail"
-import PreviewPrice from "./price"
-
-export default async function ProductPreview({
-  productPreview,
-  isFeatured,
-  region,
-}: {
-  productPreview: ProductPreviewType
-  isFeatured?: boolean
-  region: Region
+export default async function ProductPreview(props: {
+  productPreview: ProductPreviewType;
+  isFeatured?: boolean;
+  region: Region;
 }) {
+  const { productPreview, isFeatured, region } = props;
+
   const pricedProduct = await retrievePricedProductById({
     id: productPreview.id,
     regionId: region.id,
-  }).then((product) => product)
+  }).then((product) => product);
 
   if (!pricedProduct) {
-    return null
+    return null;
   }
 
   const { cheapestPrice } = getProductPrice({
     product: pricedProduct,
     region,
-  })
+  });
 
   return (
     <LocalizedClientLink
@@ -51,5 +47,5 @@ export default async function ProductPreview({
         </div>
       </div>
     </LocalizedClientLink>
-  )
+  );
 }
